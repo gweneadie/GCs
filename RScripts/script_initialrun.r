6b44ad1c96caf61d3baa521d7a6addab68f52780
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-01-20 14:42:14 -0500

    test run using adaptive tuning

--- RScripts/script_initialrun.r
+++ RScripts/script_initialrun.r
@@ -23,14 +23,26 @@ newinitpars = runinit$chain[ nrow(runinit$chain), ]
 runanother = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,
                  N = 1e3,
                  transform.pars = transform.func,
-                 priorfuncs = list( singleunif.prior, singleunif.prior, .prior, gaus.prior ),
+                 priorfuncs = list( singleunif.prior, singleunif.prior, lnorm.prior, gaus.prior ),
                  ppars = list( gbounds, phi0bounds, Mpars, rhbounds ),
                  propDF = mypropDF, 
-                 covmat = newcovmat, parnames = c("Phi_0", "gamma", "alpha", "beta")
+                 covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
 )
 
 # plot again
 plot( as.mcmc( transform.chains(runanother$chain) ) )
 
 runanother$acceptance.rate
+
+test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 1e3, yourpatience = 20, initcovmat = newcovmat, initlogpars = log(initpars),
+                      mydat = mydata, logDF = logDF.limepy, 
+                      priors = prior.wrapper,
+                      transform.pars = transform.func,
+                      priorfuncs = list( singleunif.prior, singleunif.prior, lnorm.prior, gaus.prior ),
+                      ppars = list( gbounds, phi0bounds, Mpars, rhbounds ),
+                      propDF = mypropDF, 
+                      parnames = c("g", "Phi_0", "M", "r_h"))
+
+plot( as.mcmc( transform.chains(test$chain)))
+
 
