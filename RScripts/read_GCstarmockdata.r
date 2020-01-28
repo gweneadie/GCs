65be85f7031c4fc6e293c440ada01b6bf41dc189
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-01-28 15:57:50 -0500

    received second set of mock data from Jeremy

--- RScripts/read_GCstarmockdata.r
+++ RScripts/read_GCstarmockdata.r
@@ -2,12 +2,18 @@
 
 GCstars = read.csv("../mockdata/snap.csv", header=FALSE, col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
 
+GCstars = GCstars = read.table("../mockdata/snap_version2.dat", col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
+
 # columns 2, 3, 4 are x, y, z in GC-centered coordinates
 # columns 5, 6, 7,  are v_x, v_y, v_z in GC-centered coordinates
 
 # code takes in r and v
 mydata = data.frame( r = with( GCstars, expr = sqrt(x^2 + y^2 + z^2) ), v = with( GCstars, expr = sqrt(vx^2 + vy^2 + vz^2) ) )
 
+mydata2 = data.frame( r = with( GCstars, expr = sqrt(x^2 + y^2 + z^2) ), v = with( GCstars, expr = sqrt(vx^2 + vy^2 + vz^2) ) )
+
 saveRDS(object = mydata, file = paste("../mockdata/snap_Rformat_", Sys.Date(), ".rds", sep=""))
+
+saveRDS(object = mydata2, file = paste("../mockdata/snap_version2_", Sys.Date(), ".rds", sep=""))
 
         
