0baf3fa55dcb480416e7fd3b5e2b093268cf64f3
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-01-28 17:36:48 -0500

    took away transform of parameters

--- RScripts/script_initialrun.r
+++ RScripts/script_initialrun.r
@@ -4,26 +4,27 @@ source("script_set-up-simGCstars.r")
 
 library(coda)
 # initial run
-runinit = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,N = 5e2, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = covariancematrix
+runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,N = 5e2, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = covariancematrix
 )
 
 # look at the parameter chains
-plot( as.mcmc( transform.chains(runinit$chain)) )
+plot( as.mcmc( runinit$chain) ) 
 
 newcovmat = cov( runinit$chain )
 
 newinitpars = runinit$chain[ nrow(runinit$chain), ]
 
-runanother = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 1e3, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
+runanother = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 1e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
 )
 
 # plot again
-plot( as.mcmc( transform.chains(runanother$chain) ) )
+plot( as.mcmc( runanother$chain) )
 
 runanother$acceptance.rate
 
-test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 500, yourpatience = 20, initcovmat = newcovmat, initlogpars = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, parnames = c("g", "Phi_0", "M", "r_h"))
+test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 500, yourpatience = 20, initcovmat = newcovmat, initlogpars = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, parnames = c("g", "Phi_0", "M", "r_h"))
 
-plot( as.mcmc( transform.chains(test$chain)))
+plot( as.mcmc( test$chain[5001:1e4, ]) )
 
+nrow(test$chain)
 
