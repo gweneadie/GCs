d8c34fc95b91245bf0d1040f5c05867cdadd454f
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-01-28 15:58:21 -0500

    got rid of extra white space in function calls

--- RScripts/script_initialrun.r
+++ RScripts/script_initialrun.r
@@ -4,13 +4,7 @@ source("script_set-up-simGCstars.r")
 
 library(coda)
 # initial run
-runinit = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,
-              N = 5e2,
-              transform.pars = transform.func,
-              priorfuncs = list( singleunif.prior, singleunif.prior, lnorm.prior, gaus.prior ),
-              ppars = list( gbounds, phi0bounds, Mpars, rhbounds ),
-              propDF = mypropDF, 
-              covmat = covariancematrix
+runinit = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,N = 5e2, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = covariancematrix
 )
 
 # look at the parameter chains
@@ -20,13 +14,7 @@ newcovmat = cov( runinit$chain )
 
 newinitpars = runinit$chain[ nrow(runinit$chain), ]
 
-runanother = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,
-                 N = 1e3,
-                 transform.pars = transform.func,
-                 priorfuncs = list( singleunif.prior, singleunif.prior, lnorm.prior, gaus.prior ),
-                 ppars = list( gbounds, phi0bounds, Mpars, rhbounds ),
-                 propDF = mypropDF, 
-                 covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
+runanother = GCmcmc(init = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 1e3, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
 )
 
 # plot again
@@ -34,14 +22,7 @@ plot( as.mcmc( transform.chains(runanother$chain) ) )
 
 runanother$acceptance.rate
 
-test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 1e3, yourpatience = 20, initcovmat = newcovmat, initlogpars = log(initpars),
-                      mydat = mydata, logDF = logDF.limepy, 
-                      priors = prior.wrapper,
-                      transform.pars = transform.func,
-                      priorfuncs = list( singleunif.prior, singleunif.prior, lnorm.prior, gaus.prior ),
-                      ppars = list( gbounds, phi0bounds, Mpars, rhbounds ),
-                      propDF = mypropDF, 
-                      parnames = c("g", "Phi_0", "M", "r_h"))
+test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 500, yourpatience = 20, initcovmat = newcovmat, initlogpars = log(initpars), mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, transform.pars = transform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, parnames = c("g", "Phi_0", "M", "r_h"))
 
 plot( as.mcmc( transform.chains(test$chain)))
 
