9bf60397bd89516e53df5cef5510b41110319040
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-04-09 17:41:04 -0400

    added mock data that has bound only particles

--- RScripts/read_GCstarmockdata.r
+++ RScripts/read_GCstarmockdata.r
@@ -1,19 +1,28 @@
 # read in mock data and save as an R object in the correct format for GCmcmc
-
 GCstars = read.csv("../mockdata/snap.csv", header=FALSE, col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
 
-GCstars = GCstars = read.table("../mockdata/snap_version2.dat", col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
+GCstars2 = read.table("../mockdata/snap_version2.dat", col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
+
+GCstars3 = read.table("../mockdata/snap_g15phi05m1e5rh3.dat", col.names = c("mass", "x", "y", "z", "vx", "vy", "vz", "ID", "notsure"))
 
 # columns 2, 3, 4 are x, y, z in GC-centered coordinates
 # columns 5, 6, 7,  are v_x, v_y, v_z in GC-centered coordinates
 
 # code takes in r and v
-mydata = data.frame( r = with( GCstars, expr = sqrt(x^2 + y^2 + z^2) ), v = with( GCstars, expr = sqrt(vx^2 + vy^2 + vz^2) ) )
+# use function to calculuate distance and speed
+source("function_distancespeed.r")
 
-mydata2 = data.frame( r = with( GCstars, expr = sqrt(x^2 + y^2 + z^2) ), v = with( GCstars, expr = sqrt(vx^2 + vy^2 + vz^2) ) )
+mydata = distancespeed(GCstars)
+
+mydata2 = distancespeed(GCstars2)
+
+mydata3 = distancespeed(GCstars3)
+
+
 
 saveRDS(object = mydata, file = paste("../mockdata/snap_Rformat_", Sys.Date(), ".rds", sep=""))
 
 saveRDS(object = mydata2, file = paste("../mockdata/snap_version2_", Sys.Date(), ".rds", sep=""))
 
-        
+saveRDS(object = mydata3, file = paste("../mockdata/snap_g15phi05m1e5rh3", Sys.Date(), ".rds", sep=""))
+
