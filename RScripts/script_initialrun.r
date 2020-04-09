2089f6ada9388a7826e06fd8293143e77258da62
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-04-09 17:42:10 -0400

    removed unneccessary white space

--- RScripts/script_initialrun.r
+++ RScripts/script_initialrun.r
@@ -3,9 +3,11 @@
 source("script_set-up-simGCstars.r")
 
 library(coda)
+
 # initial run
-runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper,N = 5e2, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = covariancematrix
-)
+runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 5e2, 
+                 transform.pars = notransform.func, 
+                 priorfuncs = list( singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, log10Mpars, rhbounds ), propDF = mypropDF, covmat = covariancematrix)
 
 # look at the parameter chains
 plot( as.mcmc( runinit$chain) ) 
@@ -14,7 +16,7 @@ newcovmat = cov( runinit$chain )
 
 newinitpars = runinit$chain[ nrow(runinit$chain), ]
 
-runanother = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 1e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
+runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, N = 1e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, log10Mpars, rhbounds ), propDF = mypropDF, covmat = newcovmat, parnames = c("g", "Phi_0", "M", "r_h")
 )
 
 # plot again
@@ -22,9 +24,15 @@ plot( as.mcmc( runanother$chain) )
 
 runanother$acceptance.rate
 
-test = adjustproposal(acceptrange = c(0.4,0.45), Nsteps = 500, yourpatience = 20, initcovmat = newcovmat, initlogpars = initpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, l10norm.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, Mpars, rhbounds ), propDF = mypropDF, parnames = c("g", "Phi_0", "M", "r_h"))
+newinitpars = runanother$chain[ nrow(runanother$chain), ]
 
-plot( as.mcmc( test$chain[5001:1e4, ]) )
 
-nrow(test$chain)
+test = adjustproposal(acceptrange = c(0.26,0.42), Nsteps = 500, yourpatience = 15, initcovmat = newcovmat, initlogpars = newinitpars, mydat = mydata, logDF = logDF.limepy, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( gbounds, phi0bounds, log10Mpars, rhbounds ), propDF = mypropDF, parnames = c("g", "Phi_0", "M", "r_h"))
 
+plot( as.mcmc(test$chain))
+
+
+
+saveRDS(object = test, file = paste("../results/prelim_mcmc_", Sys.Date()))
+
+        
