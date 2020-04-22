c1a39adc1f226bad60e394ba40aa0c2a8371c8be
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-04-22 16:14:54 -0400

    added try function to catch error returned from spes

--- RScripts/function_logDFspes.r
+++ RScripts/function_logDFspes.r
@@ -18,10 +18,22 @@ logDF.spes = function(pars, dat, transform.pars=NULL, pot=NULL, DF=NULL){
   
   # pars[1] = phi0 ; pars[2] = B ; pars[3] = eta, pars[4] = M, pars[5] = rh
   
-  lmodel = limepy$spes(phi0 = pars[1], B = pars[2], eta = pars[3], M = pars[4], rh=pars[5])
+  lmodel = try(limepy$spes(phi0 = pars[1], B = pars[2], eta = pars[3], M = pars[4], rh=pars[5]), silent=TRUE)
  
-  # code takes in r, v in GC-centered coordinates
-  output = log( lmodel$df( dat$r, dat$v ) )
+  # if there is an error
+  if( any(class(lmodel)=="try-error") ){
+    
+    output = -Inf
+    
+  }else{
+    
+    # code takes in r, v in GC-centered coordinates
+    output = try(log( lmodel$df( dat$r, dat$v ) ), silent = TRUE)
+    if( any(class(output)=="try-error") ){
+      output = -Inf
+    }
+    
+  }
 
   output
   
