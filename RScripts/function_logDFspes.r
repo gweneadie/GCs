87d6705fe6a3afbf741cdb5636e9e6a46ecc344e
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-04-22 16:33:04 -0400

    removed arguments not needed and added more comments

--- RScripts/function_logDFspes.r
+++ RScripts/function_logDFspes.r
@@ -4,12 +4,13 @@ use_python("/usr/bin/python3")
 limepy <- import("limepy")
 
 
-# function to take in limepy$df function and dat.try
+##### function to take in limepy$df function and dat.try
+# pars: vector of model parameter values
+# dat: dataframe of data
+# transform.pars: list of functionc to transform parameters
+ 
 
-# GME logPDF function format:
-# logPDF( pars=init, dat=dat.current, DF=DF, pot=pot, transform.pars=transform.pars )
-
-logDF.spes = function(pars, dat, transform.pars=NULL, pot=NULL, DF=NULL){
+logDF.spes = function(pars, dat, transform.pars=NULL){
  
   # transform parameters 
   if( !is.null( transform.pars ) ){
@@ -20,15 +21,16 @@ logDF.spes = function(pars, dat, transform.pars=NULL, pot=NULL, DF=NULL){
   
   lmodel = try(limepy$spes(phi0 = pars[1], B = pars[2], eta = pars[3], M = pars[4], rh=pars[5]), silent=TRUE)
  
-  # if there is an error
+  # if there is an error then return -Inf
   if( any(class(lmodel)=="try-error") ){
     
     output = -Inf
     
   }else{
     
-    # code takes in r, v in GC-centered coordinates
+    # if there is not an error, then pass data (r, v in GC-centered coordinates) to df
     output = try(log( lmodel$df( dat$r, dat$v ) ), silent = TRUE)
+    
     if( any(class(output)=="try-error") ){
       output = -Inf
     }
