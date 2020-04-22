b4494441ac0fab1bc795469dc3104ada5b60350c
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-04-22 16:20:48 -0400

    running with narrow prior on rh but 0,1 on B and eta

--- RScripts/script_initialrun-SPES.r
+++ RScripts/script_initialrun-SPES.r
@@ -5,10 +5,10 @@ source("script_set-up-SPES_simGCstars.r")
 library(coda)
 
 # initial run
-runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 5e3, 
+runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e2, 
                  transform.pars = notransform.func, 
                  priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ),
-                 propDF = mypropDFspes, covmat = covariancematrix
+                 propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h")
 )
 
 # look at the parameter chains
@@ -18,7 +18,7 @@ newcovmat = cov( runinit$chain )
 
 newinitpars = runinit$chain[ nrow(runinit$chain), ]
 
-runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h")
+runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e4, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h")
 )
 
 # plot again
@@ -28,14 +28,19 @@ runanother$acceptance.rate
 
 newinitpars = runanother$chain[ nrow(runanother$chain), ]
 
+newcovmat = cov( runanother$chain )
 
-test = adjustproposal(acceptrange = c(0.26,0.42), Nsteps = 500, yourpatience = 15, initcovmat = newcovmat, initlogpars = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, parnames = cc("Phi_0", "B", "eta", "M", "r_h"))
+# run with thinning
+runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e5, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h"), thinning = 10)
+
+
+test = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 10, initcovmat = newcovmat, initlogpars = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
 
 plot( as.mcmc(test$chain))
 
 
 
-saveRDS(object = test, file = paste("../results/prelim_mcmc_", Sys.Date()))
+saveRDS(object = runanother, file = paste("../results/prelim_mcmc_narrow-rh-prior_rmin5", Sys.Date()))
 
         
 
