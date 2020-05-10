cf096ad709998ceded3d1b0faa1329b3f626c5a3
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-05-10 13:01:37 -0400

    made negative r and negative M illegal in likelihood, before calling

--- RScripts/function_logDFspes.r
+++ RScripts/function_logDFspes.r
@@ -4,10 +4,11 @@ use_python("/usr/bin/python3")
 limepy <- import("limepy")
 
 
-##### function to take in limepy$df function and dat.try
-# pars: vector of model parameter values
-# dat: dataframe of data
-# transform.pars: list of functionc to transform parameters
+##### distribution function from the SPES model
+# pars: vector of model parameter values that must be in this order:
+      # pars[1] = phi0 ; pars[2] = B ; pars[3] = eta, pars[4] = M, pars[5] = rh
+# dat: dataframe of data with numeric columns distance r and speed v
+# transform.pars: list of functionc to transform parameters if sampling in a difference space
  
 
 logDF.spes = function(pars, dat, transform.pars=NULL){
@@ -17,21 +18,26 @@ logDF.spes = function(pars, dat, transform.pars=NULL){
     pars = transform.pars( pars )
   }
   
-  # pars[1] = phi0 ; pars[2] = B ; pars[3] = eta, pars[4] = M, pars[5] = rh
+  # if the half-light radius or total mass parameters are negative, then return -Inf for every data point
+  if(pars[5]<0 | pars[4]<0){ return( rep( -Inf, nrow(dat) ) ) }
   
+  # numerically determine the likelihood given the parameter values
   lmodel = try(limepy$spes(phi0 = pars[1], B = pars[2], eta = pars[3], M = pars[4], rh=pars[5]), silent=TRUE)
  
-  # if there is an error then return -Inf
+  # if there is an error because of bad parameter values (i.e. unphysical model) then return -Inf
   if( any(class(lmodel)=="try-error") ){
     
+    browser()
     output = -Inf
     
   }else{
     
-    # if there is not an error, then pass data (r, v in GC-centered coordinates) to df
+    # if there is not an error, then calculate likelihood of the data (r, v in GC-centered coordinates)
     output = try(log( lmodel$df( dat$r, dat$v ) ), silent = TRUE)
     
+    # if there is an error then return -Inf
     if( any(class(output)=="try-error") ){
+      browser()
       output = -Inf
     }
     
