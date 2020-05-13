9a7cb9631700683b1e069e747432eeb4b93d951f
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-05-13 17:34:49 -0400

    removed set seed

--- RScripts/script_initialrun-SPES.r
+++ RScripts/script_initialrun-SPES.r
@@ -5,42 +5,31 @@ source("script_set-up-SPES_simGCstars.r")
 library(coda)
 
 # initial run
-runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 5e2, 
+runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 5e3, 
                  transform.pars = notransform.func, 
-                 priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ),
-                 propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h")
-)
+                 priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, rhGaussianPrior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ),
+                 propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
 
-# look at the parameter chains
-plot( as.mcmc( runinit$chain) ) 
+# look at the parameter chains if you want
+plot( as.mcmc( runinit$chain) )
 
-newcovmat = cov( runinit$chain )
-
-newinitpars = runinit$chain[ nrow(runinit$chain), ]
-
-runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 5e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h")
-)
-
-# plot again
-plot( as.mcmc( runanother$chain) )
-
-runanother$acceptance.rate
-
-newinitpars = runanother$chain[ nrow(runanother$chain), ]
-
-newcovmat = cov( runanother$chain )
-
-# run with thinning
-runanother = GCmcmc(init = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e4, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h"), thinning = 10)
+# look at the "chain" of values of the likelihood*prior at every step
+plot( as.mcmc( runinit$logDFchain) )
 
 
-test = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 10, initcovmat = newcovmat, initlogpars = newinitpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, gaus.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), propDF = mypropDFspes, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
+# adjust covariance matrix for proposal distribution using automated 
+results = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 10, initialrun = runinit,
+                      mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, transform.pars = notransform.func, 
+                      priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, rhGaussianPrior ), 
+                      ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhbounds ), 
+                      propDF = mypropDFspes, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
 
-plot( as.mcmc(test$chain))
+
+plot(as.mcmc(results$chain))
 
 
-
-saveRDS(object = runanother, file = paste("../results/prelim_mcmc_narrow-rh-prior_normin", Sys.Date()))
+# save the chain
+saveRDS(object = results, file = paste("../results/prelim_mcmc_narrow-rh-prior_normin", nsamp, Sys.Date(), sep="_"))
 
         
 
