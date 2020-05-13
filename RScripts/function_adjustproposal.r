dc9ba42d0a645ead7b35dee893c5f34c486d1baf
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-05-13 13:23:57 -0400

    took out initial run, instead pass initial run

--- RScripts/function_adjustproposal.r
+++ RScripts/function_adjustproposal.r
@@ -1,13 +1,12 @@
 ### adaptive tuning function 
 
-# acceptrange: vector of length 2 with lower and upper bounds of "acceptable" acceptance rate
-# yourpatience: how many iterations of the adaptive algorithm you are willing to wait
-# Nsteps: interger, number of steps in each iteration
-# initcovmat: the initial covariance matrix to be used for the proposal distribution
-# initpars: vector of initial paramter values
-# ...: other aguments to be passed to GCmcmc
+# acceptrange:    vector of length 2 with lower and upper bounds of "acceptable" acceptance rate
+# yourpatience:   how many iterations of the adaptive algorithm you are willing to wait
+# Nsteps:         interger, number of steps in each iteration
+# initialrun:    a list that was returned from an initial run of GCmcmc()
+# ...:            other aguments to be passed to GCmcmc
 
-adjustproposal = function(acceptrange = c(0.2,0.4), yourpatience, Nsteps, initcovmat, initpars, ... ){
+adjustproposal = function(acceptrange = c(0.2,0.4), yourpatience, Nsteps, initialrun, ... ){
   
   # start patience count
   count = 1
@@ -16,43 +15,32 @@ adjustproposal = function(acceptrange = c(0.2,0.4), yourpatience, Nsteps, initco
   accept.lower <- acceptrange[1]
   accept.upper <- acceptrange[2]
   
-   # run the chain for a bit, using the initial covariance matrix for the parameters
-  initialrun = GCmcmc(N = Nsteps, covmat = initcovmat, init = initpars, ...)
-  
-  # change to new initial values (i.e. where chain stopped)
-  newinitpars <- as.numeric( initialrun$chain[Nsteps, ] )
-  
-  # new covariance matrix from initial run
-  newcovmat = cov(initialrun$chain)
-  
-  # check acceptance rates
+    # check acceptance rates
   goodacceptancerate = (initialrun$acceptance.rate < accept.upper) & (initialrun$acceptance.rate > accept.lower)
-      
-  if( goodacceptancerate ){ combinedrun = initialrun$chain}
-    
+  
+  # save only chain to use in next part
+  initialchain = initialrun$chain
+  
   # while the acceptance rate is bad and the count is less than your patience, or if this is the first iteration.
   while( (!goodacceptancerate) && (count<yourpatience) | count==1 ){
     
-    # add to the count for your patrience
-    count = 1 + count
+    # change to new initial values (i.e. where chain stopped)
+    newinitpars <- as.numeric( initialchain[nrow(initialchain), ] )
+    
+    # new covariance matrix from initial run
+    newcovmat = cov(initialchain)
     
     # run a new chain, starting where the last one stopped and with the new covariance matrix for the proposal
     newrun = GCmcmc(N = Nsteps, covmat = newcovmat, init = newinitpars, ...)
     
-    # save new initial values (i.e. where chain stopped)
-    newinitpars <- as.numeric( newrun$chain[Nsteps, ] )
-
+    # check acceptance rate
+    goodacceptancerate = (newrun$acceptance.rate < accept.upper) & (newrun$acceptance.rate > accept.lower)
     
-    if( count==2 ){
-      # combine initial chain and new chain together
-      combinedrun = rbind(initialrun$chain, newrun$chain)
-    }else{
-      # combine new chain with all previous old chains
-      combinedrun = rbind(combinedrun, newrun$chain)
-    }
+    # combine new run with previous runs
+    initialchain = rbind(initialchain, newrun$chain)
     
-    # get new covariance matrix using all previous runs
-    newcovmat = cov(combinedrun)
+    # add to the count for your patrience
+    count = 1 + count
     
     # print the count for the user so they know what's happening
     print(paste("count =", count, sep=" "))
@@ -60,7 +48,7 @@ adjustproposal = function(acceptrange = c(0.2,0.4), yourpatience, Nsteps, initco
   }
   
   # output everything of possible value in a list
-  out = list(newpropsd = newcovmat, chain = combinedrun, patiencemeter = count, newinitpars = newinitpars, acceptancerate = newrun$acceptance.rate)
+  out = list(newpropsd = newcovmat, chain = initialchain, lastchain = newrun, patiencemeter = count, newinitpars = newinitpars, acceptancerate = newrun$acceptance.rate)
   
   out
   
