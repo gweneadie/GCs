192a59657a847b998922a11f9495cfdea1f277ae
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-06-26 18:30:56 -0400

    made labels on test plots easier to see

--- RScripts/script_initialrun-SPES.r
+++ RScripts/script_initialrun-SPES.r
@@ -11,14 +11,11 @@ runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = p
                  propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
 
 # look at the parameter chains if you want
-plot( as.mcmc( runinit$chain) )
-
-# look at the "chain" of values of the likelihood*prior at every step
-plot( as.mcmc( runinit$logDFchain) )
+plot( as.mcmc( runinit$chain), cex.lab=2, cex.main=2, cex.axis=2 )
 
 
 # adjust covariance matrix for proposal distribution using automated 
-results = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 10, initialrun = runinit,
+results = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 20, initialrun = runinit,
                       mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, transform.pars = notransform.func, 
                       priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, truncnorm.prior ), 
                       ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhpars ), 
@@ -27,7 +24,14 @@ results = adjustproposal(acceptrange = c(0.2,0.4), Nsteps = 500, yourpatience = 
 
 plot(as.mcmc(results$chain))
 
+# with proposal distribution set, run a longer chain
+finalrun = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 5e3, 
+                 transform.pars = notransform.func, 
+                 priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, truncnorm.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhpars ),
+                 propDF = mypropDFspes, covmat = newcovmat, parnames = c("Phi_0", "B", "eta", "M", "r_h"), thinning = 2)
 
+
+plot(as.mcmc(finalrun$chain))
 # save the chain
 saveRDS(object = runinit, file = paste("../results/prelim_mcmc_truncnorm-rh-prior_normin", nsamp, Sys.Date(), sep="_"))
 
