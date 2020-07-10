0bbc0992e6152efa50e8b4a18a911f4c63c0e077
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-07-10 14:03:19 -0400

    took out annoying whitespace

--- RScripts/script_initialrun-SPES.r
+++ RScripts/script_initialrun-SPES.r
@@ -5,10 +5,7 @@ source("script_set-up-SPES_simGCstars.r")
 library(coda)
 
 # initial run
-runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e3, 
-                 transform.pars = notransform.func, 
-                 priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, truncnorm.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhpars ),
-                 propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
+runinit = GCmcmc(init = initpars, mydat = mydata, logDF = logDF.spes, priors = prior.wrapper, N = 1e3, transform.pars = notransform.func, priorfuncs = list( singleunif.prior, singleunif.prior, singleunif.prior, normlog10M.prior, truncnorm.prior ), ppars = list( phi0bounds, Bbounds, etabounds, log10Mpars, rhpars ), propDF = mypropDFspes, covmat = covariancematrix, parnames = c("Phi_0", "B", "eta", "M", "r_h"))
 
 # look at the parameter chains if you want
 plot( as.mcmc( runinit$chain), cex.lab=2, cex.main=2, cex.axis=2 )
