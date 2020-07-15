50c3b234e814e87f4de0cf4509af848ff33cd6ce
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-07-15 16:49:47 -0400

    removed excess white space

--- RScripts/function_logDFlimepy.r
+++ RScripts/function_logDFlimepy.r
@@ -12,22 +12,16 @@ limepy <- import("limepy")
 logDF.limepy = function(pars, dat, transform.pars=NULL, pot=NULL, DF=NULL){
  
   # transform parameters 
-  if( !is.null( transform.pars ) ){
-    pars = transform.pars( pars )
-  }
+  if( !is.null( transform.pars ) ){  pars = transform.pars( pars ) }
   
-    # if the half-light radius or total mass parameters are negative, then return -Inf for every data point
-  if(pars[4]<0 | pars[3]<0){ return( rep( -Inf, nrow(dat) ) ) }
+  # if the half-light radius or total mass parameters are negative, then return -Inf for every data point
+  if(pars[3]<0 | pars[4]<0){ return( rep( -Inf, nrow(dat) ) ) }
   
   # numerically determine the df given the parameter values
-  # pars[1] = g ; pars[2] = phi0 ; pars[3] = M, pars[4] = rh
+  # here, pars[1] = g ; pars[2] = phi0 ; pars[3] = M, pars[4] = rh
   lmodel = try( limepy$limepy(g=pars[1], phi0=pars[2], M=pars[3], rh=pars[4]), silent = TRUE)
   
-  if( any(class(lmodel)=="try-error") ){
-    
-    output = rep( -Inf, nrow(dat) )
-    
-  }else{
+  if( any(class(lmodel)=="try-error") ){ output = rep( -Inf, nrow(dat) ) }else{
     
     # calculate log-likelihood (log( DF/M_total) ), using GC-centered coordinates
     output = try( log( ( lmodel$df( dat$r, dat$v ) )/pars[3] ), silent=TRUE )
