c21d4b3df407042720773b0f31979ac74d1ecd0e
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-07-15 15:53:12 -0400

    removed unused functions lnorm.prior, etc

--- RScripts/function_priors.r
+++ RScripts/function_priors.r
@@ -12,12 +12,6 @@ lnorm.prior <- function( pars, ppars ){
   
 }
 
-########### Log10-Normal prior
-l10norm.prior <- function( pars, ppars ){
-  
-  log10(exp(1))  / ( pars*ppars[2]*sqrt(2*pi) ) * exp( - ( log10(pars) - ppars[1] )^2 / (2*ppars[2]^2)  )
-  
-}
 
 ########### Normal prior on log10M
 normlog10M.prior <- function(pars, ppars ){
@@ -36,22 +30,12 @@ singleunif.prior <- function( pars, ppars ){
   par.max = ppars[2]
   
   # if parameter is held fixed...
-  if( par.min==par.max ){
-    priorvalue = 1
-  }else{
-    # calculated uniform prior value
+  if( par.min==par.max ){ priorvalue = 1 }else{
+    # calculated uniform prior value at pars
     priorvalue = 1/(par.max - par.min) * ( par.min <= pars  &  pars <= par.max )
   }
   
   priorvalue
-  
-}
-
-rhGaussianPrior <- function( pars, ppars ){
-  
-  if( pars<=0 ){ return(0) }else{
-    dnorm(x = pars, mean = ppars[1], sd = ppars[2])
-  }
   
 }
 
