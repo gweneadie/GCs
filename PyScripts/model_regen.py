d8d7201975be70e8497c9f001799f23e2d574f31
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-09-21 09:30:03 -0400

    sorted out libraries, made 50 average GCs

--- PyScripts/model_regen.py
+++ PyScripts/model_regen.py
@@ -7,7 +7,7 @@ from limepy import spes
 import numpy as np
 
 #Set number of models you wish to run
-ngen=1
+ngen=50
 
 #Set mass, radius, g, and phi0 for model
 m=1.e5
@@ -18,6 +18,6 @@ phi0=5.
 for i in range(0,ngen):
 	model=limepy(g=g,phi0=phi0,M=m,rh=r)
 	data=sample(model,N=int(m))
-	filename='../mockdata/paper1data/m%sr%sg%sphi%s_%s.dat' % (str(int(np.log10(m))),str(int(r)),str(g),str(phi0),str(i))
+	filename='../mockdata/paper1data/Regen/m%sr%sg%sphi%s_%s.dat' % (str(int(np.log10(m))),str(int(r)),str(g),str(phi0),str(i))
 	print(filename)
 	np.savetxt(filename,np.column_stack([data.x,data.y,data.z,data.vx,data.vy,data.vz]))
