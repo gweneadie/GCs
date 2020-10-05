510377635584861f1da077ce83c4d69501ce1910
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-10-05 12:43:28 -0400

    random seed fix to generate different data sets. Updated wrong script initially. Regen needed updating, not gen

--- PyScripts/model_regen.py
+++ PyScripts/model_regen.py
@@ -9,6 +9,9 @@ import numpy as np
 #Set number of models you wish to run
 ngen=50
 
+#Do you wish to randomize the dataset?
+random_seed=True 
+
 #Set mass, radius, g, and phi0 for model
 m=1.e5
 r=3.
@@ -17,7 +20,11 @@ phi0=5.
 
 for i in range(0,ngen):
 	model=limepy(g=g,phi0=phi0,M=m,rh=r)
-	data=sample(model,N=int(m))
+	
+	if random_seed:
+      data=sample(model,N=int(m),seed=np.random.randint(0,1000))
+  else:
+      data=sample(model,N=int(m))
 	filename='../mockdata/paper1data/Regen/m%sr%sg%sphi%s_%s.dat' % (str(int(np.log10(m))),str(int(r)),str(g),str(phi0),str(i))
 	print(filename)
 	np.savetxt(filename,np.column_stack([data.x,data.y,data.z,data.vx,data.vy,data.vz]))
