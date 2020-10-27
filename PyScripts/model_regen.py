c37243e0c1b334836ef9f662f021b02153f17f0b
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-10-27 10:41:45 -0400

    fixed white space issues

--- PyScripts/model_regen.py
+++ PyScripts/model_regen.py
@@ -1,9 +1,7 @@
 import sys
 sys.path.append('/home/Gwenny/Documents/Work/Research/GCs/limepy/')
 
-from limepy import limepy
-from limepy import sample
-from limepy import spes
+from limepy import limepy, sample, spes
 import numpy as np
 
 #Set number of models you wish to run
@@ -19,12 +17,12 @@ g=1.5
 phi0=5.
 
 for i in range(0,ngen):
-	model=limepy(g=g,phi0=phi0,M=m,rh=r)
-	
-	if random_seed:
-      data=sample(model,N=int(m),seed=np.random.randint(0,1000))
-  else:
-      data=sample(model,N=int(m))
-	filename='../mockdata/paper1data/Regen/m%sr%sg%sphi%s_%s.dat' % (str(int(np.log10(m))),str(int(r)),str(g),str(phi0),str(i))
-	print(filename)
-	np.savetxt(filename,np.column_stack([data.x,data.y,data.z,data.vx,data.vy,data.vz]))
+    model=limepy(g=g,phi0=phi0,M=m,rh=r)
+    if random_seed:
+        data=sample(model,N=int(m),seed=np.random.randint(0,1000))
+    else:
+        data=sample(model,N=int(m))
+            
+    filename='../mockdata/paper1data/Regen/m%sr%sg%sphi%s_%s.dat' % (str(int(np.log10(m))),str(int(r)),str(g),str(phi0),str(i))
+    print(filename)
+    np.savetxt(filename,np.column_stack([data.x,data.y,data.z,data.vx,data.vy,data.vz]))
