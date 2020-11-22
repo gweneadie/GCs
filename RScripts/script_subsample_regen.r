405b0f706fd4c4ae9e2aa5ba83323d822ddbbbfb
Gwendolyn Eadie <gwen.eadie@gmail.com>
2020-11-22 18:03:17 -0500

    subsampling simulated GCs, many files

--- RScripts/script_subsample_regen.r
+++ RScripts/script_subsample_regen.r
@@ -2,12 +2,12 @@
 
 
 # list of mockdata files from Regen
-filelist <- list.files(path = "../mockdata/paper1data/Regen/", pattern="m*.rds")
+filelist <- list.files(path = "../mockdata/paper1data/CompactGC/", pattern="m*.rds")
 
 # sample data set
 nsamp = 500
 # only sample stars beyond this radius
-rcut = 3
+rcut = 0
   
 
 #' get a random sample of stars
@@ -16,7 +16,7 @@ set.seed(123)
 for(i in 1:length(filelist)){
   
   # read in snap data
-  alldata = readRDS( paste0("../mockdata/paper1data/Regen/", filelist[i]) )
+  alldata = readRDS( paste0("../mockdata/paper1data/CompactGC/", filelist[i]) )
   
   # of the stars beyond rcut, sample 500 randomly
   mydata <- alldata[alldata$r > rcut, ]
@@ -25,6 +25,6 @@ for(i in 1:length(filelist)){
   mydata = mydata[sample(x = 1:nrow(mydata), size = nsamp, replace = FALSE), ]
   
   # save file
-  saveRDS(mydata, file = paste0("../mockdata/paper1data/Regen/subsamp500_outer/subsamp500outer_", filelist[i]))
+  saveRDS(mydata, file = paste0("../mockdata/paper1data/CompactGC/subsamp500/subsamp500_", filelist[i]))
   
 }
