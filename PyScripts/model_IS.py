144a60a29ed4c4dae7793cb7088c4a38df781161
Josh Speagle <jspeagle@cfa.harvard.edu>
2020-12-03 15:53:34 -0500

    speagle python scripts
    Additional Python scripts for working with GC modelling with `limepy` in Python. A summary of these changes is below.
    
    Data generation:
    `model_gen_v2.py` and `model_regen_v2.py` are scripts that are patterned off of Gwen's original scripts, but with some additional flexibility to allow for anisotropic models.
    
    Optimization:
    `model_optimize.py` loads in the data, subsamples it (by default to `logn=2.7` or `n=501` stars), and then optimizes the result. There are options to allow for anisotropic models and more/less robust optimization options (differential evolution vs Nelder-Mead). This flexibility allows the user to explore how robust the optimization is, and also cases where the data are correctly modelled (data=iso, model=iso; data=aniso, model=aniso), incorrectly modelled (data=aniso, model=iso), or involve an overly-complex model (data=iso, model=aniso). This saves the MAP solution (position and log-probability) as well as the Hessian (with some safety checks to ensure it is semi-positive definite).
    
    Importance Sampling:
    `model_IS.py` reads in the results from `model_optimize.py` and performs a very simple Gaussian proposal (where the size of the covariance is just inflated relative to the default Hessian). The mean and covariance of the proposal along with samples and log-probabilities (of the proposal and posterior) are then saved. A diagnostic set of corner plots (using `corner`) are also generated.
    
    MCMC:
    `model_MCMC.py` uses the results from `model_IS.py` to sample using MCMC. This builds off of `emcee` using the Differential Evolution ensemble moves. The user can adjust the number of chains, burn-in iterations, and sampling iterations. Samples and log-probabilities (both initial Gaussian proposal and posterior) are then saved along with a diagnostic corner plot.
    
    Nested Sampling:
    `model_NS.py` uses Nested Sampling to estimate the posterior and the marginal likelihood (evidence). No additional inputs from previous modelling steps are required. This runs off of `dynesty`. The user can change the number of live points and the point at which the sampler begins to construct bounding distributions. Samples, log-weights, and the log-evidence (plus errors) are then saved along with a diagnostic plot.
    
    While the code has been tested on a simple example, it has not yet been stress-tested on a variety of parameters or model cases.

---
+++ PyScripts/model_IS.py
@@ -0,0 +1,106 @@
+import sys
+
+from limepy import limepy, sample, spes
+import numpy as np
+from scipy.optimize import minimize, differential_evolution
+import corner
+import dynesty
+
+# do you wish to include anisotropic models?
+anisotropic = False
+
+# scale factor to inflate the Normal proposal
+inflate = 2.5  # inflate std dev by this factor
+
+# number of samples to save
+nsamps = 5000
+
+# data to be loaded in
+fname = 'm5r3g1.5phi5.0'  # data file
+logn = 2.7  # log of number of stars to read in
+
+# file paths
+fpath = 'mockdata/'  # location of data to be read in
+fout = 'fits/'  # location where fits will be stored
+
+# load data
+n = int(10**logn)  # number of stars to read in
+x, y, z, vx, vy, vz = np.loadtxt(fpath + fname + '.dat')[:n].T
+nparams = 4 + anisotropic
+
+# load optimized results
+if anisotropic:
+    fopt = fpath + fout + fname + '_optim_{}_a.npy'.format(logn)
+else:
+    fopt = fpath + fout + fname + '_optim_{}.npy'.format(logn)
+with open(fopt, 'rb') as f:
+    theta_map = np.load(f)
+    theta_logp = np.load(f)
+    theta_H = np.load(f)
+
+# define utility functions
+exec(open('PyScripts/utils.py').read())
+
+# generate precision/covariance matrix
+Cinv = theta_H / inflate**2
+C = np.linalg.inv(Cinv)
+
+# generate samples from normal proposal distribution
+samples = np.random.multivariate_normal(theta_map, C, size=nsamps)
+
+# compute proposal log-probability
+const = np.linalg.slogdet(2. * np.pi * C)[1]
+logq = -0.5 * np.array([(np.dot(np.dot((s - theta_map), Cinv), s - theta_map)
+                         + const)
+                        for s in samples])
+
+# compute posterior log-probability
+logp = np.zeros(nsamps)
+for i, s in enumerate(samples):
+    if (i + 1) % 250 == 0:
+        print(i + 1)
+    logp[i] = logpost(s)
+
+# compute importance weight
+logwt = logp - logq
+wts = np.exp(logwt - np.nanmax(logwt))
+
+# save samples
+if anisotropic:
+    fsamps = fpath + fout + fname + '_IS_{}_a.npy'.format(logn)
+else:
+    fsamps = fpath + fout + fname + '_IS_{}.npy'.format(logn)
+with open(fsamps, 'wb') as f:
+    np.save(f, theta_map)
+    np.save(f, C)
+    np.save(f, samples)
+    np.save(f, logq)
+    np.save(f, logp)
+
+# define labels for cornerplots
+if anisotropic:
+    labels = [r'$\log M$', r'$r_h$', r'$g$', r'$\Phi_0$', r'$r_a$']
+else:
+    labels = [r'$\log M$', r'$r_h$', r'$g$', r'$\Phi_0$']
+
+# save proposal cornerplot (no weights)
+corner.corner(samples,  # generated samples
+              levels=[0.68, 0.95, 0.997],  # 1, 2, 3-sigma contours
+              labels=labels,  # axis labels
+              show_titles=True)
+if anisotropic:
+    plt.savefig(fpath + fout + fname + '_IS_nowt_{}_a.png'.format(logn))
+else:
+    plt.savefig(fpath + fout + fname + '_IS_nowt_{}.png'.format(logn))
+plt.close()
+
+# save posterior cornerplot (with weights)
+corner.corner(dynesty.utils.resample_equal(samples, wts),  # resampled points
+              levels=[0.68, 0.95, 0.997],  # 1, 2, 3-sigma contours
+              labels=labels,  # axis labels
+              show_titles=True)
+if anisotropic:
+    plt.savefig(fpath + fout + fname + '_IS_wt_{}_a.png'.format(logn))
+else:
+    plt.savefig(fpath + fout + fname + '_IS_wt_{}.png'.format(logn))
+plt.close()
