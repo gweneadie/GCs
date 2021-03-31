3501bec260c34d278ec5702e6e728a920656f558
gweneadie <gweneadie@gmail.com>
2021-03-31 14:52:59 -0400

    plotting results from biased sampling

---
+++ RScripts/plot_massprofile_biasedsampling.R
@@ -0,0 +1,83 @@
+# script to make 3x3 grid of mass profiles for paper
+
+# we will plot the 13th file as an example for each case
+example = 13
+
+# make a vector of the folders in the order that you want them plotted
+resultsfolders <- c("RegenOutsideCore/", "RegenInsideCore/",
+                    "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                    "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_Inner/")
+
+mockdatafolders <- c("RegenAll/subsamp500_outsidecore/", "RegenAll/subsamp500_insidecore/",
+                     "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                     "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/")
+
+plotTitle <- c("Outside Core", "Inside Core","","","","")
+outermarginleft <- c("Average", "", "Compact", "", "Extended", "")
+outermarginbottom <- c(rep("", 4), "r (pc)", "r (pc)")
+
+# set a character string vectors for the y-axes and x-axes
+Ylab = expression(M(r<R)~(10^5~M['\u0298']))
+Ylab = c(Ylab, "", Ylab, "", Ylab, "")
+Xlab = ""
+
+# set the y and x range for all plots
+yrange = list(c(0,1.5), c(0,1.5), c(0,1.5))
+xrange = list(c(0,15), c(0,8), c(0,30))
+
+# colour for true mass profile
+truecol = "red"
+# colour for mean mass profile
+meancol = "lightblue"
+# true total mass
+truetotalcol = "darkgreen"
+# colour for posterior mass profiles
+postcol = rgb(0,0,0,0.006)
+postlegend = "black"
+
+# open file to write to
+pdf(file = paste0("../Figures/massprofiles_biasedsampling", Sys.Date(), ".pdf"), width = 6, height = 6)
+
+# set up the outer margins, inner margins, grid, etc.
+par(mfrow=c(3,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
+
+for(i in 1:length(resultsfolders)){
+  
+  # get ID for the GC
+  filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
+  ID <- strsplit(x = filename, split = ".rds")[[1]][1]
+  
+  # load the particular data set used
+  mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
+  
+  # load the true parameter values
+  truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
+  
+  # get the filename for the mass profiles from this GC
+  resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("massprofiles_chain_limepy_", ID))
+  # load the mass profile posterior samples
+  results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
+  
+  # make the plot!
+  plot(results[[1]]$r, results[[1]]$mass/1e5, xlab=Xlab, ylab=Ylab[i], xlim = xrange[[ceiling(i/2)]], ylim=yrange[[ceiling(i/2)]], type="n", main="", las=1)
+  
+  grid()
+  
+  mtext(text = outermarginleft[i], side = 2, line=5)
+  mtext(text = outermarginbottom[i], side = 1, line=3, cex=0.75)
+  mtext(text = plotTitle[i], side = 3, line=1)
+  
+  # add all the different mass profiles from the posterior
+  lapply(X = results, FUN = function(X) lines(X$r, X$mass/1e5, col=rgb(0,0,0,0.006)) )
+  
+  # abline(h=truepars[2]/1e5, col="darkgreen", lty=3, lwd=3)
+  
+  # calculate true model and add to plot true model mass profile
+  # truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
+  # lines(truemodel$r, truemodel$mc, col="red", lwd=2)
+  
+}
+
+
+# turn off device
+dev.off()
