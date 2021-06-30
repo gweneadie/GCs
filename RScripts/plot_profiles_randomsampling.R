f2f70c30f5a82f626db28d344072a911b042f623
gweneadie <gweneadie@gmail.com>
2021-06-30 18:37:41 -0400

    put all profiles for mean square velocity into one figure

--- RScripts/plot_profiles_randomsampling.R
+++ RScripts/plot_profiles_randomsampling.R
@@ -5,16 +5,6 @@ source("function_logLike_LIMEPY.r")
 # we will plot the 13th file as an example for each case
 example = 13
 
-####### 1. Plot MASS PROFILES for the average, compact, and extended ##########
-
-# make a vector of the folders in the order that you want them plotted
-resultsfolders <- c("RegenAll/", "RegenCompact/", "RegenExtended/")
-
-mockdatafolders <- c("RegenAll/subsamp500_random/", "CompactGC/subsamp500/", "ExtendedGC/subsamp500/")
-
-# plot titles
-plotTitle <- c("Average", "Compact", "Extended")
-
 # set a character string vectors for the y-axes and x-axes
 Ylab = expression(M(r<R)~(10^5~M['\u2609']))
 Ylabvel = expression(bar(v^2))
@@ -24,7 +14,8 @@ Xlab = "r (pc)"
 # set the y and x range for all plots
 yrange = list(c(0,1.5), c(0,1.5), c(0,1.5))
 xrange = list(c(0,15), c(0,8), c(0,30))
-yrangevel = list(c(0,100),c(0,300),c(0,50))
+yrangevelPhi0 = list(c(0,100),c(0,300),c(0,50), c(0,150), c(0,150))
+xrangevelPhi0 = list(c(0,20), c(0,9), c(0,40), c(0,35), c(c(0,16)))
 
 # colour for true mass profile
 truecol = "red"
@@ -35,6 +26,19 @@ truetotalcol = "darkgreen"
 # colour for posterior mass profiles
 postcol = rgb(0,0,0,0.006)
 postlegend = "black"
+
+# plot titles
+plotTitle <- c("Average", "Compact", "Extended", expression("High"~Phi[0]), expression("Low"~Phi[0]))
+
+####### 1. Plot MASS PROFILES for the average, compact, and extended ##########
+
+# make a vector of the folders in the order that you want them plotted
+resultsfolders <- c("RegenAll/", "RegenCompact/", "RegenExtended/")
+
+mockdatafolders <- c("RegenAll/subsamp500_random/", "CompactGC/subsamp500/", "ExtendedGC/subsamp500/")
+
+# plot titles
+plotTitle <- c("Average", "Compact", "Extended")
 
 # open file to write to
 png(filename = paste0("../Figures/massprofiles_randomsampling", Sys.Date(), ".png"), res=300, width = 8, height = 3, units = "in")
@@ -85,14 +89,24 @@ for(i in 1:length(resultsfolders)){
 # turn off device
 dev.off()
 
-########### 2. Plot velocity profiles for Average, Compact and Extended #######
+####### 2. Plot velocity profiles for Average, Compact and Extended #######
+
+# make a vector of the folders in the order that you want them plotted
+resultsfolders <- c("RegenAll/", "RegenCompact/", "RegenExtended/", "Regen_highPhi0/", "Regen_lowPhi0/")
+
+mockdatafolders <- c("RegenAll/subsamp500_random/", "CompactGC/subsamp500/", "ExtendedGC/subsamp500/", "Regen_highPhi0/subsamp500/", "Regen_lowPhi0/subsamp500/")
+
 # open file to write to
-pdf(paste0("../Figures/velocityprofiles_randomsampling_", Sys.Date(), ".pdf"), width = 8, height = 3)
+pdf(paste0("../Figures/velocityprofiles_randomsampling_", Sys.Date(), ".pdf"), width = 9, height = 8)
 
 # set up the outer margins, inner margins, grid, etc.
-par(mfrow=c(1,3), oma=c(0,1,3,3), mai=c(1,0.6,0,0))
+par(oma=c(0,1,3,3), mai=c(1,0.6,0,0))
 
-for(i in 1:length(resultsfolders)){
+
+layout(mat = matrix(c(1,1,2,2,3,3,
+                      0,4,4,5,5,0), nrow = 2, byrow = TRUE))
+
+for(i in 1:3){
   
   # get ID for the GC
   filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
@@ -110,16 +124,49 @@ for(i in 1:length(resultsfolders)){
   results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
   
   # make the plot!
-  plot(results[[1]]$r, results[[1]]$v2, xlab=Xlab, ylab=Ylabvel[i], xlim = xrange[[i]], ylim=yrangevel[[i]],  type="n", main="", las=1)
+  plot(results[[1]]$r, results[[1]]$v2, xlab=Xlab, ylab=Ylabvel[i], xlim = xrangevelPhi0[[i]], ylim=yrangevelPhi0[[i]],  type="n", main="", las=1)
   
   grid()
   
   mtext(text = plotTitle[i], side = 3, line=1)
   
   # if it's the last plot, then add a legend
-  if(i==length(resultsfolders)){
+  if(i==1){
     legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2)
   }
+  
+  # add all the different mass profiles from the posterior
+  lapply(X = results, FUN = function(X) lines(X$r, X$v2, col=postcol) )
+  
+  # calculate true model and add to plot true model mass profile
+  truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
+  lines(truemodel$r, truemodel$v2, col="red", lwd=2)
+  
+}
+
+for(i in 4:length(resultsfolders)){
+  
+  # get ID for the GC
+  filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
+  ID <- strsplit(x = filename, split = ".rds")[[1]][1]
+  
+  # load the particular data set used
+  mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
+  
+  # load the true parameter values
+  truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
+  
+  # get the filename for the mass profiles from this GC
+  resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("velocityprofiles_chain_limepy_", ID))
+  # load the velocity profile posterior samples
+  results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
+  
+  # make the plot!
+  plot(results[[1]]$r, results[[1]]$v2, xlab=Xlab, ylab=Ylabvel[i], xlim = xrangevelPhi0[[i]], ylim=yrangevelPhi0[[i]],  type="n", main="", las=1)
+  
+  grid()
+  
+  mtext(text = plotTitle[i], side = 3, line=1)
   
   # add all the different mass profiles from the posterior
   lapply(X = results, FUN = function(X) lines(X$r, X$v2, col=postcol) )
@@ -135,7 +182,7 @@ dev.off()
 
 
 
-########### 3. Plot the high Phi0 and low Phi0 results ######################
+####### 3. Plot MASS PROFILES for the high Phi0 and low Phi0 results #######
 
 # make a vector of the folders in the order that you want them plotted
 resultsfolders <- c("Regen_highPhi0/", "Regen_lowPhi0/")
@@ -146,7 +193,7 @@ plotTitle <- c(expression("High"~Phi[0]), expression("Low"~Phi[0]))
 Ylab = c(expression(M(r<R)~(10^5~M['\u2609'])), "")
 
 # open file to write to
-pdf(paste0("../Figures/massprofiles_diffPhi0_randomsampling_", Sys.Date(), ".pdf"), width = 5.3333, height = 3)
+pdf(paste0("../Figures/massprofiles_diffPhi0_randomsampling_", Sys.Date(), ".pdf"), width = 9, height = 3)
 
 # set up the outer margins, inner margins, grid, etc.
 par(mfrow=c(1,2), oma=c(0,1,3,3), mai=c(1,0.6,0,0))
@@ -181,6 +228,56 @@ for(i in 1:length(resultsfolders)){
   # calculate true model and add to plot true model mass profile
   truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
   lines(truemodel$r, truemodel$mc/1e5, col="red", lwd=2)
+  
+}
+
+# turn off device
+dev.off()
+
+
+
+####### 4. Plot velocity profiles for high Phi0 and low Phi0 results ###########
+
+pdf(paste0("../Figures/velocityprofiles_randomsampling_diffPhi0_", Sys.Date(), ".pdf"), width = 9, height = 4)
+
+# set up the outer margins, inner margins, grid, etc.
+par(mfrow=c(1,2), oma=c(0,1,3,3), mai=c(1,0.6,0,0))
+
+for(i in 1:length(resultsfolders)){
+  
+  # get ID for the GC
+  filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
+  ID <- strsplit(x = filename, split = ".rds")[[1]][1]
+  
+  # load the particular data set used
+  mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
+  
+  # load the true parameter values
+  truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
+  
+  # get the filename for the mass profiles from this GC
+  resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("velocityprofiles_chain_limepy_", ID))
+  # load the velocity profile posterior samples
+  results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
+  
+  # make the plot!
+  plot(results[[1]]$r, results[[1]]$v2, xlab=Xlab, ylab=Ylabvel[i], xlim = xrangevelPhi0[[i]], ylim=yrangevelPhi0[[i]],  type="n", main="", las=1)
+  
+  grid()
+  
+  mtext(text = plotTitle[i], side = 3, line=1)
+  
+  # if it's the last plot, then add a legend
+  if(i==length(resultsfolders)){
+    legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2)
+  }
+  
+  # add all the different mass profiles from the posterior
+  lapply(X = results, FUN = function(X) lines(X$r, X$v2, col=postcol) )
+  
+  # calculate true model and add to plot true model mass profile
+  truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
+  lines(truemodel$r, truemodel$v2, col="red", lwd=2)
   
 }
 
