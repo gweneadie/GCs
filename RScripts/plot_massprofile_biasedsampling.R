cdcf230bb8196c173faf358377118fe882d719d7
gweneadie <gweneadie@gmail.com>
2021-07-09 18:06:40 -0400

    put Phi0 on same plot, took out solar symbol

--- RScripts/plot_massprofile_biasedsampling.R
+++ RScripts/plot_massprofile_biasedsampling.R
@@ -3,31 +3,43 @@ library(Cairo)
 source("function_logLike_LIMEPY.r")
 
 # we will plot the 13th file as an example for each case
-example = 13
+example = 15
 
-########## first plot the mass profiles for the cases when r_h changes #########
+########## plot the mass profiles for different r_h and Phi_0 #########
 
 # make a vector of the folders in the order that you want them plotted
 resultsfolders <- c("RegenOutsideCore/", "RegenInsideCore/",
                     "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
-                    "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/")
+                    "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
+                    "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
+                    "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
 
 mockdatafolders <- c("RegenAll/subsamp500_outsidecore/", "RegenAll/subsamp500_insidecore/",
                      "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
-                     "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/")
+                     "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
+                     "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
+                     "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
 
-plotTitle <- c("Outside Core", "Inside Core","","","","")
-outermarginleft <- c("Average", "", "Compact", "", "Extended", "")
-outermarginbottom <- c(rep("", 4), "r (pc)", "r (pc)")
+plotTitle <- c("Outside Core", "Inside Core", rep("",7))
+
+outermarginleft <- c("Average", "", "Compact", "", "Extended", "",
+                     expression("High "~Phi[0]), "",
+                     expression("Low "~Phi[0]), ""
+                     )
+
+outermarginbottom <- c(rep("", 8), "r (pc)", "r (pc)")
 
 # set a character string vectors for the y-axes and x-axes
-Ylab = expression(M(r<R)~(10^5~M['\u2609']))
-Ylab = c(Ylab, "", Ylab, "", Ylab, "")
-Xlab = ""
+Ylab = expression(M(r<R)~(10^5~M["sun"]))
+Ylab = rep( c(Ylab, "") , 5)
+Xlab = rep("", 10)
 
 # set the y and x range for all plots
-yrange = list(c(0,1.5), c(0,1.5), c(0,1.5))
-xrange = list(c(0,15), c(0,8), c(0,30))
+yrange = list(c(0,1.5), c(0,1.5), c(0,1.5), c(0,1.5), c(0,1.5))
+xrange = list(c(0,20), c(0,7.5), c(0,45), c(0,40), c(c(0,12.5)))
+
+# yrange = list(c(0,1.5), c(0,1.5), c(0,1.5))
+# xrange = list(c(0,15), c(0,8), c(0,30))
 
 # colour for true mass profile
 truecol = "red"
@@ -40,10 +52,10 @@ postcol = rgb(0,0,0,0.006)
 postlegend = "black"
 
 # open file to write to
-png(filename = paste0("../Figures/massprofiles_biasedsampling", Sys.Date(), ".png"), res=300, width = 6, height = 6, units = "in")
+pdf(paste0("../Figures/massprofiles_biasedsampling", Sys.Date(), ".pdf"), width = 6, height = 8)
 
 # set up the outer margins, inner margins, grid, etc.
-par(mfrow=c(3,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
+par(mfrow=c(5,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
 
 for(i in 1:length(resultsfolders)){
   
@@ -84,7 +96,7 @@ for(i in 1:length(resultsfolders)){
   
   # if it's the upper-right plot, then add a legend
   if(i==2){
-    legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2)
+    legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2, cex = 0.75)
   }
   
   
@@ -98,68 +110,78 @@ dev.off()
 
 ########## next plot the mass profiles for the cases when phi_0 changes ########
 
-resultsfolders <- c("Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
-                    "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
+# # plot the 15th file in this case
+# example = 15
+# 
+# resultsfolders <- c("Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
+#                     "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
+# 
+# mockdatafolders <- resultsfolders
+# 
+# # labels for title, margins, axis margins
+# plotTitle <- c("Outside Core", "Inside Core","","")
+# outermarginleft <- c(expression("High"~Phi[0]), "", expression("Low"~Phi[0]), "")
+# outermarginbottom <- c(rep("", 2), "r (pc)", "r (pc)")
+# Ylab = expression(M(r<R)~(10^5~M['sun']))
+# Ylab = c(Ylab, "", Ylab, "")
+# 
+# yrange = list(c(0,1.5), c(0,1.5), c(0,1.5), c(0,1.5))
+# xrange = list(c(0,40), c(0,16))
 
-mockdatafolders <- resultsfolders
-
-# labels for title, margins, axis margins
-plotTitle <- c("Outside Core", "Inside Core","","")
-outermarginleft <- c(expression("High"~Phi[0]), "", expression("Low"~Phi[0]), "")
-outermarginbottom <- c(rep("", 2), "r (pc)", "r (pc)")
-Ylab = expression(M(r<R)~(10^5~M['\u2609']))
-Ylab = c(Ylab, "", Ylab, "")
-
-# open file to write to
-png(filename = paste0("../Figures/massprofiles_diffPhi0_biasedsampling_", Sys.Date(), ".png"), res=300, width = 6, height = 4, units = "in")
-
-# set up the outer margins, inner margins, grid, etc.
-par(mfrow=c(2,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
-
-for(i in 1:length(resultsfolders)){
-  
-  # get ID for the GC
-  filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
-  ID <- strsplit(x = filename, split = ".rds")[[1]][1]
-  
-  # load the particular data set used
-  mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
-  
-  # load the true parameter values
-  truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
-  
-  # get the filename for the mass profiles from this GC
-  resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("massprofiles_chain_limepy_", ID))
-  # load the mass profile posterior samples
-  results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
-  
-  # make the plot!
-  plot(results[[1]]$r, results[[1]]$mass/1e5, xlab=Xlab, ylab=Ylab[i], xlim = xrange[[ceiling(i/2)]], ylim=yrange[[ceiling(i/2)]], type="n", main="", las=1)
-  
-  grid()
-  
-  mtext(text = outermarginleft[i], side = 2, line=5)
-  mtext(text = outermarginbottom[i], side = 1, line=3, cex=0.75)
-  mtext(text = plotTitle[i], side = 3, line=1)
-  
-  # add all the different mass profiles from the posterior
-  lapply(X = results, FUN = function(X) lines(X$r, X$mass/1e5, col=rgb(0,0,0,0.006)) )
-  
-  # abline(h=truepars[2]/1e5, col="darkgreen", lty=3, lwd=3)
-  
-  # calculate true model and add to plot true model mass profile
-  truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
-  
-  # add curve of true model
-  lines(truemodel$r, truemodel$mc/1e5, col="red", lwd=2)
-  
-  # if it's the upper-right plot, then add a legend
-  if(i==2){
-    legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2)
-  }
-  
-  
-}
-
-dev.off()
-
+# # open file to write to
+# # png(filename = paste0("../Figures/massprofiles_diffPhi0_biasedsampling_", Sys.Date(), ".png"), res=300, width = 6, height = 4, units = "in")
+# pdf(file = paste0("../Figures/massprofiles_diffPhi0_biasedsampling_", Sys.Date(), ".pdf"), width = 6, height = 4)
+# 
+# # set up the outer margins, inner margins, grid, etc.
+# par(mfrow=c(2,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
+# 
+# for(i in 1:length(resultsfolders)){
+#   
+#   # get ID for the GC
+#   filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
+#   ID <- strsplit(x = filename, split = ".rds")[[1]][1]
+#   
+#   # load the particular data set used
+#   mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
+#   
+#   # load the true parameter values
+#   truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
+#   
+#   # get the filename for the mass profiles from this GC
+#   resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("massprofiles_chain_limepy_", ID))
+#   # load the mass profile posterior samples
+#   results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
+#   
+#   if(i==3){browser()}
+#   
+#   # make the plot!
+#   plot(results[[1]]$r, results[[1]]$mass/1e5, xlab="", ylab=Ylab[i], xlim = xrange[[ceiling(i/2)]], ylim=yrange[[ceiling(i/2)]], type="n", main="", las=1)
+#   
+#   grid()
+#   
+#   mtext(text = outermarginleft[i], side = 2, line=5)
+#   mtext(text = outermarginbottom[i], side = 1, line=3, cex=0.75)
+#   mtext(text = plotTitle[i], side = 3, line=1)
+#   
+#   # add all the different mass profiles from the posterior
+#   # lapply(X = results, FUN = function(X) lines(X$r, X$mass/1e5, col=rgb(0,0,0,0.006)) )
+#   for(j in 1:length(results)){
+#     lines(results[[j]]$r, results[[j]]$mass/1e5, col=rgb(0,0,0,0.006))
+#   }
+#   
+#   # calculate true model and add to plot true model mass profile
+#   truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
+#   
+#   # add curve of true model
+#   lines(truemodel$r, truemodel$mc/1e5, col="red", lwd=2)
+#   
+#   # if it's the upper-right plot, then add a legend
+#   if(i==2){
+#     legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2)
+#   }
+#   
+#   
+# }
+# 
+# dev.off()
+# 
