df6fa52cf2fd4e5f0dad5c6ed3c61454a910b775
gweneadie <gweneadie@gmail.com>
2021-07-20 11:22:27 -0400

    added code for velocity profile, moved preamble info before plotting

--- RScripts/plot_massprofile_biasedsampling.R
+++ RScripts/plot_massprofile_biasedsampling.R
@@ -5,24 +5,25 @@ source("function_logLike_LIMEPY.r")
 # we will plot the 13th file as an example for each case
 example = 15
 
-########## plot the mass profiles for different r_h and Phi_0 #########
 
 # make a vector of the folders in the order that you want them plotted
-resultsfolders <- c("RegenOutsideCore/", "RegenInsideCore/",
-                    "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+resultsfolders <- c("CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                    "RegenOutsideCore/", "RegenInsideCore/",
                     "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
                     "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
                     "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
 
-mockdatafolders <- c("RegenAll/subsamp500_outsidecore/", "RegenAll/subsamp500_insidecore/",
-                     "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+mockdatafolders <- c("CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                     "RegenAll/subsamp500_outsidecore/", "RegenAll/subsamp500_insidecore/",
                      "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
                      "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
                      "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
 
 plotTitle <- c("Outside Core", "Inside Core", rep("",7))
 
-outermarginleft <- c("Average", "", "Compact", "", "Extended", "",
+outermarginleft <- c( "Compact", "",
+                      "Average", "",
+                      "Extended", "",
                      expression("High "~Phi[0]), "",
                      expression("Low "~Phi[0]), ""
                      )
@@ -33,24 +34,22 @@ outermarginbottom <- c(rep("", 8), "r (pc)", "r (pc)")
 Ylab = expression(M(r<R)~(10^5~M["sun"]))
 Ylab = rep( c(Ylab, "") , 5)
 Xlab = rep("", 10)
+Ylabvel = c(expression(bar(v^2)), "", "", expression(bar(v^2)), "")
 
 # set the y and x range for all plots
 yrange = list(c(0,1.5), c(0,1.5), c(0,1.5), c(0,1.5), c(0,1.5))
-xrange = list(c(0,20), c(0,7.5), c(0,45), c(0,40), c(c(0,12.5)))
+xrange = list(c(0,7.5), c(0,20), c(0,45), c(0,40), c(c(0,12.5)))
+yrangevel = list(c(0,300), c(0,100),c(0,50), c(0,150), c(0,150))
+xrangevel = list( c(0,9), c(0,20), c(0,45), c(0,40), c(c(0,16)))
 
-# yrange = list(c(0,1.5), c(0,1.5), c(0,1.5))
-# xrange = list(c(0,15), c(0,8), c(0,30))
-
-# colour for true mass profile
+# colour for profile
 truecol = "red"
-# colour for mean mass profile
-meancol = "lightblue"
-# true total mass
-truetotalcol = "darkgreen"
 # colour for posterior mass profiles
 postcol = rgb(0,0,0,0.006)
 postlegend = "black"
 
+
+########## plot the mass profiles for different r_h and Phi_0 #########
 # open file to write to
 pdf(paste0("../Figures/massprofiles_biasedsampling", Sys.Date(), ".pdf"), width = 6, height = 8)
 
@@ -106,6 +105,61 @@ for(i in 1:length(resultsfolders)){
 # turn off device
 dev.off()
 
+
+########## plot the mean square VELOCITY profiles for different r_h and Phi_0 #######
+
+# open file to write to
+pdf(paste0("../Figures/velocityprofiles_biasedsampling_", Sys.Date(), ".pdf"), width = 6, height = 8)
+# set up the outer margins, inner margins, grid, etc.
+par(mfrow=c(5,2), oma=c(2,5,3,5), mai=c(0.5,0.6,0,0))
+
+for(i in 1:length(resultsfolders)){
+  
+  # get ID for the GC
+  filename <- list.files(path = paste0("../mockdata/paper1data/", mockdatafolders[i]))[example]
+  ID <- strsplit(x = filename, split = ".rds")[[1]][1]
+  
+  # load the particular data set used
+  mydata <- readRDS(paste0("../mockdata/paper1data/", mockdatafolders[i], filename))
+  
+  # load the true parameter values
+  truepars <- readRDS(paste0("../results/paper1results/", resultsfolders[i], "truepars.rds"))
+  
+  # get the filename for the mass profiles from this GC
+  resultsfile <- list.files(path = paste0("../results/paper1results/", resultsfolders[i]), pattern = paste0("velocityprofiles_chain_limepy_", ID))
+  # load the mass profile posterior samples
+  results <- readRDS(paste0("../results/paper1results/", resultsfolders[i],  resultsfile))
+  
+  # make the plot!
+  plot(results[[1]]$r, results[[1]]$v2, 
+       xlab=Xlab, ylab=Ylabvel[i], 
+       xrangevel[[ceiling(i/2)]], ylim=yrangevel[[ceiling(i/2)]],
+       type="n", main="", las=1)
+  
+  grid()
+  
+  mtext(text = plotTitle[i], side = 3, line=1)
+  mtext(text = outermarginleft[i], side = 2, line=5)
+  mtext(text = outermarginbottom[i], side = 1, line=3, cex=0.75)
+
+  # add all the different mass profiles from the posterior
+  lapply(X = results, FUN = function(X) lines(X$r, X$v2, col=postcol) )
+  
+  # calculate true model and add to plot true model mass profile
+  truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
+  lines(truemodel$r, truemodel$v2, col="red", lwd=2)
+  
+  # if it's the upper-right plot, then add a legend
+  if(i==2){
+    legend("topright", legend = c("posterior profiles", "true profile"), col=c(postlegend, truecol), lty=1, lwd=2, cex = 0.75)
+  }
+  
+  
+}
+
+
+# turn off device
+dev.off()
 
 
 ########## next plot the mass profiles for the cases when phi_0 changes ########
