61267b1d04fe6118fd01809de8405672a4c4283c
gweneadie <gweneadie@gmail.com>
2021-07-23 23:56:25 -0400

    filenames more automated

--- RScripts/plot_summarystatistics.r
+++ RScripts/plot_summarystatistics.r
@@ -1,13 +1,37 @@
 library(coda)
 library(Cairo)
 
-foldername = "Regen_lowPhi0"
-plottitle = "____ GCs, stars randomly sampled"
+# make a vector of the folders in the order that you want them plotted
+resultsfolders <- c("CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                    "RegenOutsideCore/", "RegenInsideCore/",
+                    "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
+                    "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
+                    "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
+
+mockdatafolders <- c("CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
+                     "RegenAll/subsamp500_outsidecore/", "RegenAll/subsamp500_insidecore/",
+                     "ExtendedGC/subsamp500_outer/", "ExtendedGC/subsamp500_inner/",
+                     "Regen_highPhi0/subsamp500_outer/", "Regen_highPhi0/subsamp500_inner/",
+                     "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
+
+outin <- c("stars sampled outside core", "stars sampled inside core")
+
+plottitles <- c(paste0("Compact GCs, ", outin),
+                paste0("Average GCs, ", outin),
+                paste0("Extended GCs, ", outin),
+                paste0("High ", expression(Phi[0]), ", ", outin),
+                paste0("Low ", expression(Phi[0]), ", ", outin))
+                    
+whichone <- 7  
+
+foldername = resultsfolders[whichone]
+title = plottitles[whichone]
+
 # if you want to set ranges explicitly
-grange = c(1,2)
-Phi0range = c(1,4)
-Mrange = c(9e4, 1.1e5)
-rhrange  =c(2, 4) # need to change for each type of cluster if using this
+grange = c(0,3)
+Phi0range = c(1,8)
+Mrange = c(0.9, 1.3)
+rhrange  =c(0, 6) # need to change for each type of cluster if using this
 
 # plot 50% or 95%??
 # thisquant <- c("X25.", "X75.")
@@ -67,23 +91,26 @@ xrange <- function(param, dframe=df, truep=truepars, interval = c("X25.", "X75."
 }
 
 
-# pdf(paste0("../Figures/", foldername, "_limepy_subsamp500_interquartiles_", Sys.Date(), ".pdf"), width=9, height=7)
-
-windowsFonts(SolarSymbol = windowsFont("Arial Unicode MS"))
-
-pdf(paste0("../Figures/", foldername, "_limepy_subsamp500_", quantname, "_", Sys.Date(), ".pdf"), width=9, height=7, family="SolarSymbol")
+######### plot the quantiles for every GC ##############
+pdf(paste0("../Figures/", foldername, "_limepy_subsamp500_", quantname, "_", Sys.Date(), ".pdf"), width=9, height=7)
 
 
 # set up plotting area
-par(mfrow=c(1,4), oma=c(0,0,5,0))
+par(oma=c(5,5,5,3), mai=c(0,0,0,0), cex.lab=xfactor, cex.axis=xfactor)
 
-par(mar=c(5,5,2,0))
+layout(mat = matrix(c(1,2,3,4), nrow = 1, byrow = TRUE))
+
+par(mar=c(0,1.5,3,0))
 
 
-with(df, plot(Mean[Parameter=="g"], y, type="n", panel.first = TRUE, xlab = "g", cex.lab=xfactor, cex.axis=xfactor, ylab="GC id", yaxt="n",
+with(df, plot(Mean[Parameter=="g"], y,
+              type="n", panel.first = TRUE,
+              xlab = "g", cex.lab=xfactor, cex.axis=xfactor, yaxt="n",
               # xlim = xrange(param="g",interval = thisquant), 
               xlim = grange,
               main = bquote(.(gwithin)/50) ))
+
+mtext(text = "GC ID", side = 2, outer = TRUE, line = 3)
 
 if( foldername=="Regen_highPhi0"){
   title(expression("High"~Phi[0]~" GCs, stars randomly sampled"), outer = TRUE, line = -0.05)
@@ -103,7 +130,7 @@ quants(df, parameter="g", length=0.1, quantiles = thisquant)
 
 # Phi_0
 
-par(mar=c(5,2,2,2))
+# par(mar=c(5,2,2,2))
 
 with(df, plot(Mean[Parameter=="Phi_0"], y, type="n", panel.first = TRUE, xlab = expression(Phi[0]), cex.lab=xfactor, cex.axis=xfactor, ylab="",yaxt="n", 
               # xlim = xrange("Phi_0", interval = thisquant), 
@@ -116,9 +143,9 @@ with(df, points(df$Mean[Parameter=="Phi_0"], y) )
 quants(df, parameter="Phi_0", length=0.1, quantiles = thisquant)
 
 # M
-par(mar=c(5,2,2,2))
+# par(mar=c(5,2,2,2))
 
-with(df, plot(Mean[Parameter=="M"], y, type="n", panel.first = TRUE, xlab = expression(M[total]~(M["?"])), cex.lab=xfactor, cex.axis=xfactor, ylab="",yaxt="n", xaxt="n", 
+with(df, plot(Mean[Parameter=="M"]/1e5, y, type="n", panel.first = TRUE, xlab = expression(M[total]~(10^5~M["sun"])), cex.lab=xfactor, cex.axis=xfactor, ylab="",yaxt="n", xaxt="n", 
               # xlim = xrange("M", interval = thisquant), 
               xlim = Mrange,
               main = bquote(.(Mwithin)/50)) )
@@ -129,7 +156,7 @@ with(df, points(df$Mean[Parameter=="M"], y) )
 quants(df, parameter="M", length=0.1, quantiles = thisquant)
 
 # rh
-par(mar=c(5,2,2,2))
+# par(mar=c(5,2,2,2))
 
 with(df, plot(Mean[Parameter=="r_h"], y, type="n", panel.first = TRUE, xlab = expression(r[h]~(pc)), cex.lab=xfactor, cex.axis=xfactor, ylab="",yaxt="n", 
               # xlim = xrange("r_h", interval = thisquant), 
