238d093d003d4ba908acdcf4f6b35a17960b37d9
gweneadie <gweneadie@gmail.com>
2021-10-15 10:55:15 -0400

    almost done calculating CMP differences

--- RScripts/calc_CMPdifferences.R
+++ RScripts/calc_CMPdifferences.R
@@ -2,6 +2,9 @@
 source("function_logLike_LIMEPY.r")
 source("function_profiles.r")
 
+library(dplyr)
+library(matrixStats)
+
 # make a vector of the folders in the order that you want them plotted
 resultsfolders <- c("RegenAll/", "RegenOutsideCore/", "RegenInsideCore/",
                     "RegenCompact/", "CompactGC/subsamp500_outer/", "CompactGC/subsamp500_inner/",
@@ -20,7 +23,9 @@ mockdatafolders <- c("RegenAll/subsamp500_random/", "RegenAll/subsamp500_outside
 thisquant <- c("X2.5.", "X97.5.")
 
 
-# function to calculate limepy model for every row of parameters in markov chain
+# sequence of r values for calculations and plotting
+# xrange = list(c(0,7.5), c(0,20), c(0,45), c(0,40), c(c(0,12.5)))
+rseq = seq(0,45, length.out=2e2)
 
 # for each GC type
 for(i in 1:length(resultsfolders)){
@@ -53,17 +58,50 @@ for(i in 1:length(resultsfolders)){
   # for every set of mean parameter values, calculate the mean-square velocity profile
   velest <- apply(X = meanpars, MARGIN = 1, FUN = velocityprofile)
   
-  # calculate true model and add to plot true model mass profile
+  # calculate true model
   truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
   
-  # r values 
-  plot(estimates[[i]]$r, estimates[[i]]$mass, ylim=c(0,1.2), type="l")
-  lines(truemodel$r, truemodel$mc, col="red", lwd=2)
+  
+  # for every profile estimate, create a spline function for the CMP so we can calculate at any M(r)
+  funCMPs <- lapply(X = estimates, FUN = splineprofile, columns=c("r", "mass"))
+  
+  # for the true model, create a spline function for the true CMP
+  funtrue <- splinefun(x = truemodel$r, y = truemodel$mc)
+  
+  # get estimates at custom r values
+  CMPs <- lapply(funCMPs, FUN = function(x) data.frame(r=rseq, mass=x(rseq)))
+  truemass <- funtrue(rseq)
+  
+  ############# still not working properly, need to check
+  
+  # calculate difference between true CMP and mean CMP, save values
+  diffCMPs <- lapply( CMPs, FUN = function(x) data.frame(r=x$r, diffs=(x$mass - truemass)) )
+  
+  # put all the diffs into a single dataframe so the r values match up
+  temp <- bind_cols(diffCMPs)  
+  # grab all columns with differences
+  temp <- temp %>% dplyr:: select(starts_with("diff")) 
+  # calculate stats of difference across rows (i.e., at each r)
+  diffStats <- data.frame(xbar=rowMeans(x = temp), se=rowSds(as.matrix(temp)))
+  
+  ###################
+  
+  # plot the estimated CMPs
+  plot(estimates[[i]]$r, estimates[[i]]$mass, ylim=c(0,1.2), type="l", col=rgb(0,0,0.3, 0.3))
   for(j in 2:50){
     lines(estimates[[j]]$r, estimates[[j]]$mass, col=rgb(0,0,0.3, 0.3))
   }
+  # add the true profile
+  lines(truemodel$r, truemodel$mc, col="red", lwd=2)
+  
+  plot(diffCMPs[[i]]$r, diffCMPs[[i]]$diffs, col=rgb(0,0,0.3, 0.3), ylim=c(-0.2, 0.1))
+  for(j in 2:50){
+    points(diffCMPs[[j]]$r, diffCMPs[[j]]$diffs, col=rgb(0,0,0.3, 0.3))
+  }
+  abline(h=0, col="red")
+  # make figure of 
+  
 }
 
-# calculte difference between true CMP and mean CMP, save values
 
 
