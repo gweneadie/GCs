299988c0a01adf441ac7481c0d4ba379d25a1302
gweneadie <gweneadie@gmail.com>
2021-11-05 21:44:24 -0400

    fixing axis labels, etc.

--- RScripts/plot_CMP.R
+++ RScripts/plot_CMP.R
@@ -1,12 +1,13 @@
 source("function_logLike_LIMEPY.r")
-source("function_CMPcredibleregions.R")
 source("function_profiles.r")
 
 # one sample GC results to make example CMP
 j=13
 
 # r range for each type of GC
-xrange = list(c(0,7.5), c(0.0,20), c(0,42), c(0,40), c(c(0,12.5)))
+xrangeplot = list(c(0,20), c(0,9), c(0,45), c(0,40), c(c(0,16)))
+xrange = list(c(0,20), c(0,9), c(0,45), c(0,40), c(c(0,16)))
+
 # custom r sequence for plotting
 rseq = lapply(xrange, FUN = function(x) seq(from=x[1], to=x[2], length.out=1e2))
 
@@ -14,12 +15,16 @@ regions=c(0.5, 0.75, 0.95)
 # credible region bounds
 drop.perc = (1 - regions)/2
 
-# default colours for credible region bounds
+# colours for credible region bounds and true mass profile
 solids = c("cadetblue4", "cadetblue3", "cadetblue1")
-myylab=expression( M(r<R)~(10^5~M[solar]))
-myxlab="R (kpc)"
+truecol = "red"
+
+# labels for plots and legends
+myylab = c(expression(M(r<R)~(10^5~M["sun"])), "", "", expression(M(r<R)~(10^5~M["sun"]), "") )
+myxlab="r (pc)"
 mymar = c(5,7,0.5,5)
-mycexlab=1.8
+myexp=1.35
+mylegend = c(paste0(regions*100, "% c.r."))
 
 # make a vector of the folders in the order that you want them plotted
 resultsfoldersRandom <- c("RegenAll/",
@@ -38,6 +43,15 @@ resultsfoldersBiased <- c("CompactGC/subsamp500_outer/", "CompactGC/subsamp500_i
                           "Regen_lowPhi0/subsamp500_outer/", "Regen_lowPhi0/subsamp500_inner/")
 
 
+
+png(filename = paste0("../Figures/CMPs_randomsampling_", Sys.Date(), ".png"), width = 9, height = 7, units = "in", res = 300)
+
+layout(mat = matrix(c(1,1,2,2,3,3,
+                      0,4,4,5,5,0), nrow = 2, byrow = TRUE))
+
+# set up the outer margins, inner margins, grid, etc.
+par(oma=c(0,4,3,3), mai=c(1,0.75,0,0), cex.lab=myexp, cex.axis=myexp)
+
 for(i in 1:length(resultsfoldersRandom)){
   
   # grab folder name and path
@@ -51,6 +65,7 @@ for(i in 1:length(resultsfoldersRandom)){
   chain <- allchaininfo$chain
   # convert mass to 10^5
   chain[, "M"] <- chain[, "M"]/10^5
+  
   # use every 10th row to calculate CMP
   chain <- chain[seq(from=1, to=nrow(chain), by=10), ]
   
@@ -59,28 +74,6 @@ for(i in 1:length(resultsfoldersRandom)){
   
   lower.creds <- temp[[1]]
   upper.creds <- temp[[2]]
-  
-  # # for every 10th set of parameter values in chain, calculate the mass profile
-  # CMPs <- apply(X = chain, MARGIN = 1, FUN = simplemassprofile)
-  # 
-  # # for every profile estimate, create a spline function for the CMP so we can calculate at any M(r), because limepy doesn't have this flexibility
-  # funCMPs <- lapply(X = CMPs, FUN = splineprofile, columns=c("r", "mass"))
-  # 
-  # # get estimates at custom r values
-  # r.values = rseq[[i]]
-  # customCMPs <- t(sapply(funCMPs, FUN = function(x) x(r.values)))
-  # colnames(customCMPs) = paste( "r", seq(1,length(r.values)), sep="")
-  # 
-  # # sort the M(r) values
-  # sorted.Mr = apply(X=customCMPs, MARGIN=2, FUN=sort)
-  # 
-  # lower.creds = sapply( X=drop.perc, FUN=function(x) ( sorted.Mr[ -( 1:(x*nrow(sorted.Mr)) ) , ] )[1, ] )
-  # colnames(lower.creds) = as.character(100*regions)
-  # 
-  # upper.creds = apply( X=cbind( drop.perc, regions ), MARGIN=1,
-  #                      FUN=function(x) ( sorted.Mr[ -( 1:( sum(x) * nrow( sorted.Mr ) ) ), ] )[1, ] )
-  # colnames(upper.creds) = colnames(lower.creds)
-  
   
   # load the true parameter values
   truepars <- readRDS(paste0("../results/paper1results/", resultsfoldersRandom[i], "truepars.rds"))
@@ -91,17 +84,19 @@ for(i in 1:length(resultsfoldersRandom)){
   truemodel <- limepy$limepy(g=truepars[1], phi0=truepars[2], M=truepars[3], rh=truepars[4])
   
   # r values for drawing cred. region bounds (pass to polygon)
-  poly.r = c( r.values, rev(r.values) )
+  poly.r = c(rseq[[i]], rev(rseq[[i]]) )
   
   # credible bound M(r) values for plotting (pass to polygon)
   polyM = rbind( lower.creds, upper.creds[ nrow(upper.creds):1 , ] )
   
   # r values for plotting
-  rs = c(r.values, rev(r.values))
-  Ms = rep(0, length(r.values))
+  rs = c(rseq[[i]], rev(rseq[[i]]))
+  Ms = rep(0, length(rseq[[i]]))
   
-  p = plot( r.values, (Ms), type="n", ylab="", ylim=c(0,1))
-             # xlab=xlab, ylab=ylab, cex.lab=mycexlab)
+  p = plot(rseq[[i]], (Ms), type="n",
+           xlab = myxlab, ylab=myylab[i], main = plotTitle[i],
+           ylim=c(0,1.5),
+           xlim=xrangeplot[[i]], cex.lab=myexp)
   #add a background grid
   grid()
   
@@ -110,37 +105,46 @@ for(i in 1:length(resultsfoldersRandom)){
     polygon(x=rs, y=polyM[,j], col=solids[j], border=solids[j]) 
   }
   grid()
-  lines(truemodel$r, truemodel$mc/1e5, col="red", lwd=1.5)
+  lines(truemodel$r, truemodel$mc/1e5, col=truecol, lwd=1.5)
   
-  # if( makelegend ){
-  #   # add legend without rmin/rmax
-  #   legend( "topleft",  paste(colnames(lower.creds), "%" ), bg="white",
-  #           lwd=c(8,8,8), lty=c(1,1,1), col=solids,  cex=0.8)
-  #   
-  #   # use if want to plot min/max of data position and key in legend
-  #   if( plotdataminmax ){ 
-  #     abline(v=max(dat[,"Rgc"]), lty=2)
-  #     abline(v=min(dat[,"Rgc"]), lty=2)
-  #     #covers previous legend
-  #     legend( "topleft",  c(paste(colnames(lower.creds), "%" ), expression(r[min/max~data])), bg="white",
-  #             lwd=c(8,8,8,1.3), lty=c(1,1,1,2), col=c(solids,"black"),  cex=0.8)
-  #     
-  #   }
-  # }
-  # 
+  # if it's the last plot, then add a legend
+  if(i==1){
+    legend("topright", legend = c(mylegend, "true profile"), col=c(solids, truecol), lty=1, lwd=2, bg = "white")
+  }
   
-  # add true M(r) if needed
-  # if( plottrue & !othersim ){
-  #   lines(x=r.values, y=sapply(r.values, FUN=M.r, pars=truepars), col="red", cex=1.3) 
-  # }
-  # if( plottrue & othersim ){
-  #   lines(x=r.true, y=M.true, col="red", cex=1.3)
-  # }
-  # 
-  # 
-  # print(p)
-  # 
-  # if(printpdf){  dev.off() }
-  # 
+  box()
 }
+
+dev.off()
+
+# if( makelegend ){
+#   # add legend without rmin/rmax
+#   legend( "topleft",  paste(colnames(lower.creds), "%" ), bg="white",
+#           lwd=c(8,8,8), lty=c(1,1,1), col=solids,  cex=0.8)
+#   
+#   # use if want to plot min/max of data position and key in legend
+#   if( plotdataminmax ){ 
+#     abline(v=max(dat[,"Rgc"]), lty=2)
+#     abline(v=min(dat[,"Rgc"]), lty=2)
+#     #covers previous legend
+#     legend( "topleft",  c(paste(colnames(lower.creds), "%" ), expression(r[min/max~data])), bg="white",
+#             lwd=c(8,8,8,1.3), lty=c(1,1,1,2), col=c(solids,"black"),  cex=0.8)
+#     
+#   }
+# }
+# 
+
+# add true M(r) if needed
+# if( plottrue & !othersim ){
+#   lines(x=rseq[[i]], y=sapply(rseq[[i]], FUN=M.r, pars=truepars), col="red", cex=1.3) 
+# }
+# if( plottrue & othersim ){
+#   lines(x=r.true, y=M.true, col="red", cex=1.3)
+# }
+# 
+# 
+# print(p)
+# 
+# if(printpdf){  dev.off() }
+# 
   
