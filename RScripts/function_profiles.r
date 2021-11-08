fa6e65115884af0594d703d53f6651f5c90681b5
gweneadie <gweneadie@gmail.com>
2021-11-07 20:13:57 -0500

    got rid of old mass profile function, added rhoprofile function

--- RScripts/function_profiles.r
+++ RScripts/function_profiles.r
@@ -21,43 +21,53 @@ profiles <- function(pars){
 }
 
 
-massprofile <- function(pars){
+# massprofile <- function(pars){
+#   
+#   # for a set of parameter values, calculate the model, and then estimate the cumulative mass profile
+#   
+#   # calculate the limepy model
+#   model <- limepy$limepy(g = pars[1], phi0 = pars[2], M = pars[3], rh = pars[4])
+#   
+#   nr <- length(model$r)
+#  
+#   # matrix of rs and y to integrate
+#   output <- data.frame(r=model$r, integrand=4*pi*model$rho*(model$r^2))
+#   output$mass[1] <- 0
+#   
+#   # obtain spline function that will be used to integrate
+#   myfunction <- splinefun(x = model$r, y = 4*pi*model$rho*(model$r^2), method="natural")
+#   
+#   # integrate each section to get cumulative mass profile
+#     for(i in 2:nr){
+#    output$mass[i] <- integrate(f = myfunction, lower=output$r[1], upper = model$r[i-1], subdivisions = 100)$value
+#   }
+#   
+#   # integrate using a spline, but this only gives the total
+#   #mass <- AUC(x = model$r, y = 4*pi*model$rho*(model$r^2), method = "spline", from = 0)
+#         
+#   # using trapezoidal rule for integration
+#   # trapzmass <- 4*pi* cumtrapz(x = model$r, y = as.matrix( model$rho*(model$r^2) ) )
+#   # 
+#   # # using right-hand points of bins  
+#   # RHmass <- 4*pi* cumsum(model$rho[2:nr] * ( model$r[2:nr] )^2 * diff(model$r))
+#   # 
+#   # # using left-hand points of bins  
+#   # RHmass <- 4*pi* cumsum(model$rho[1:(nr-1)] * ( model$r[1:(nr-1)] )^2 * diff(model$r))
+#   
+#   output
+#   
+# }
+
+rhoprofile <- function(pars){
   
-  # for a set of parameter values, calculate the model, and then estimate the cumulative mass profile
-  
-  # calculate the limepy model
+  # for a set of parameter values, calculate the model, and output a daraframe of the r and rho(r) profile
   model <- limepy$limepy(g = pars[1], phi0 = pars[2], M = pars[3], rh = pars[4])
   
-  nr <- length(model$r)
- 
-  # matrix of rs and y to integrate
-  output <- data.frame(r=model$r, integrand=4*pi*model$rho*(model$r^2))
-  output$mass[1] <- 0
+  data.frame(r=model$r, rho=model$rho)
   
-  # obtain spline function that will be used to integrate
-  myfunction <- splinefun(x = model$r, y = 4*pi*model$rho*(model$r^2), method="natural")
-  
-  # integrate each section to get cumulative mass profile
-    for(i in 2:nr){
-   output$mass[i] <- integrate(f = myfunction, lower=output$r[1], upper = model$r[i-1], subdivisions = 100)$value
-  }
-  
-  # integrate using a spline, but this only gives the total
-  #mass <- AUC(x = model$r, y = 4*pi*model$rho*(model$r^2), method = "spline", from = 0)
-        
-  # using trapezoidal rule for integration
-  # trapzmass <- 4*pi* cumtrapz(x = model$r, y = as.matrix( model$rho*(model$r^2) ) )
-  # 
-  # # using right-hand points of bins  
-  # RHmass <- 4*pi* cumsum(model$rho[2:nr] * ( model$r[2:nr] )^2 * diff(model$r))
-  # 
-  # # using left-hand points of bins  
-  # RHmass <- 4*pi* cumsum(model$rho[1:(nr-1)] * ( model$r[1:(nr-1)] )^2 * diff(model$r))
-  
-  output
-  
-}
-  
+} 
+
+
 velocityprofile <- function(pars){
   
   # for a set of parameter values, calculate the model, and output a dataframe of the r and mean square velocity profile
@@ -106,6 +116,27 @@ CMPcredreg <- function(CMPs, r.values, regions=c(0.5,0.75,0.95)){
   
   # put all custom CMPs into one matrix
   temp <- matrix(unlist(customCMPs), ncol = length(r.values), byrow=TRUE)
+  
+  # at every r value, get the quantiles
+  out <- t(apply(X = temp, MARGIN = 2, FUN = quantile, probs = forquantilefun))
+  
+  out
+  
+}
+
+rhocredreg <- function(rhos, r.values, regions=c(0.5,0.75,0.95)){
+  
+  # credible region bounds
+  forquantilefun = sort(c((1 - regions)/2, regions+(1 - regions)/2))
+  
+  # for every profile estimate, create a spline function for the CMP so we can calculate at any M(r), because limepy doesn't have this flexibility
+  funrhos <- lapply(X = rhos, FUN = splineprofile, columns=c("r", "rho"))
+  
+  # get estimates at custom r values
+  customrhos <- lapply(funrhos, FUN = function(x) x(r.values))
+  
+  # put all custom CMPs into one matrix
+  temp <- matrix(unlist(customrhos), ncol = length(r.values), byrow=TRUE)
   
   # at every r value, get the quantiles
   out <- t(apply(X = temp, MARGIN = 2, FUN = quantile, probs = forquantilefun))
